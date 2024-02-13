fbf6717be7c70bab9d0dd0392e0963d0bc65e219
xavivi8 <jmartinlunasescobar@gmail.com>
2024-02-13 19:04:45 +0100

    enviroment UPDATE
    - export const URL_API_FILM = 'https://api.themoviedb.org/3/search/movie?query=';
    + export const URL_API_FILM = 'https://api.themoviedb.org/3/';

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -3,7 +3,7 @@ export const environments = {
 }
 
 /* Environments de la api de Peliculas */
-export const URL_API_FILM = 'https://api.themoviedb.org/3/search/movie?query=';
+export const URL_API_FILM = 'https://api.themoviedb.org/3/';
 
 /* Environments de la api de SGE */
 export const URL_BASE = 'http://143.47.54.55/dev/app.radfpd';
