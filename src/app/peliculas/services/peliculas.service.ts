6727719f0315c10eb45a43d8893d02141ab2651c
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-13 13:06:42 +0100

    service UPDATE

--- src/app/peliculas/services/peliculas.service.ts
+++ src/app/peliculas/services/peliculas.service.ts
@@ -3,20 +3,28 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { URL_API_FILM } from 'src/environments/environments';
-import { Root } from '../interfaces/peliculas.interfaces';
+import { Pelicula, Root } from '../interfaces/peliculas.interfaces';
 import { SharedService } from '../../shared/services/shared.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeliculaService {
-
+  private peliculaSeleccionada: Pelicula | null = null;
   private urlFilm: string = URL_API_FILM;
 
   constructor(
     private http: HttpClient,
     private sharedService: SharedService
   ) { }
+
+  setPeliculaSeleccionada(pelicula: Pelicula) {
+    this.peliculaSeleccionada = pelicula;
+  }
+
+  getPeliculaSeleccionada(): Pelicula | null {
+    return this.peliculaSeleccionada;
+  }
 
   getFilmByName(name: string): Observable<Root | undefined> {
 
