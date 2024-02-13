00514db06ba0d21d30b002b1300dcd50e14a9de9
xavivi8 <jmartinlunasescobar@gmail.com>
2024-02-13 19:05:41 +0100

    pelicula service UPDATE

--- src/app/peliculas/services/peliculas.service.ts
+++ src/app/peliculas/services/peliculas.service.ts
@@ -3,8 +3,9 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { URL_API_FILM } from 'src/environments/environments';
-import { Pelicula, Root } from '../interfaces/peliculas.interfaces';
+import { Pelicula, Resultado } from '../interfaces/peliculas.interfaces';
 import { SharedService } from '../../shared/services/shared.service';
+import { ResultadoID } from '../interfaces/peliculas-id.interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -26,14 +27,24 @@ export class PeliculaService {
     return this.peliculaSeleccionada;
   }
 
-  getFilmByName(name: string): Observable<Root | undefined> {
+  getFilmByName(name: string): Observable<Resultado | undefined> {
 
-    return this.http.get<Root>(`${this.urlFilm}${name}&include_adult=false&language=es-ES&page=1`, { headers: this.sharedService.headersFilm })
+    return this.http.get<Resultado>(`${this.urlFilm}search/movie?query=${name}&include_adult=false&language=es-ES&page=1`, { headers: this.sharedService.headersFilm })
       .pipe(
         catchError(error => {
           console.error('Error:', error);
           return of(undefined);
         })
       );
+  }
+
+  getFilmById(id: number): Observable<ResultadoID | undefined>{
+    return this.http.get<ResultadoID>(`${this.urlFilm}movie/${id}`, { headers: this.sharedService.headersFilm }).pipe(
+      catchError(error => {
+        console.log('Error: ',error);
+        return of(undefined);
+
+      })
+    )
   }
 }
