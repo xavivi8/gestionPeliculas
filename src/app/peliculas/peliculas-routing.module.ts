4b864a26295c096b8d657c444ce6d754f79b9d77
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-13 13:06:59 +0100

    peliculas-routing UPDATE

--- src/app/peliculas/peliculas-routing.module.ts
+++ src/app/peliculas/peliculas-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { FavPageComponent } from './pages/fav-page/fav-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { DetallesPeliculaPageComponent } from './pages/detalles-pelicula-page/detalles-pelicula-page.component';
 
 const routes: Routes = [
   {
@@ -10,7 +11,8 @@ const routes: Routes = [
     children: [
       { path: 'home', component: HomePageComponent },
       { path: 'search', component: SearchPageComponent },
-      { path: 'fav', component: FavPageComponent }
+      { path: 'fav', component: FavPageComponent },
+      { path: 'datalles', component: DetallesPeliculaPageComponent }
     ]
   }
 ];
