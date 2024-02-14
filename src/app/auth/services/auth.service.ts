95d0bdb6a785b4a7cdf97c9dc74ca953326bba16
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-14 12:23:14 +0100

    auth service CHANGE

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,11 +1,20 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ApiResponse } from '../interfaces/usuario.interfaces';
+import { URL_API } from 'src/environments/environments';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
   ) { }
+
+  doLogin(data: any) {
+
+    const body = JSON.stringify(data);
+    return this.http.post<ApiResponse>(`${URL_API}/login.php`, body);
+  }
+
 
 }
