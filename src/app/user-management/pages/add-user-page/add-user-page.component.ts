a2c6d0b677b4741f2af0129a77f3884cad7322fd
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-15 19:19:43 +0100

    list-user CHANGE

--- src/app/user-management/pages/add-user-page/add-user-page.component.ts
+++ src/app/user-management/pages/add-user-page/add-user-page.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Rol } from '../../interfaces/rol.interface';
+import { RolsService } from '../../service/rols.service';
+import { UserService } from '../../service/user.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-add-user-page',
@@ -7,5 +14,52 @@ import { Component } from '@angular/core';
   ]
 })
 export class AddUserPageComponent {
+  usuarioForm: FormGroup = new FormGroup({});
+  roles: Rol[] = [];
 
+  constructor(public dialogRef: MatDialogRef<AddUserPageComponent>,
+              private servicioRoles: RolsService,
+              private servicioUsuario: UserService,
+              public snackBar: MatSnackBar
+  ) { }
+
+  ngOnInit() {
+    this.usuarioForm = new FormGroup({
+      usuario: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required]),
+      id_rol: new FormControl(null, [Validators.required]),
+      nombre_publico: new FormControl(null),
+      observaciones: new FormControl(null)
+    });
+
+    this.getRoles();
+  }
+
+  async getRoles() {
+    const RESPONSE = await firstValueFrom(this.servicioRoles.getAllRoles());
+    if (RESPONSE && RESPONSE.ok) {
+      this.roles = RESPONSE.data as Rol[];
+    }
+  }
+
+  async confirmAdd() {
+    if (this.usuarioForm.valid) {
+      const usuario = this.usuarioForm.value;
+
+      const RESP = await firstValueFrom(this.servicioUsuario.addUsuario(usuario));
+      if (RESP && RESP.ok) {
+        this.snackBar.open(RESP.message || '', 'Cerrar', { duration: 5000 });
+        this.dialogRef.close({ ok: RESP.ok, data: RESP.data });
+      } else {
+        this.snackBar.open(RESP?.message || 'Error al agregar usuario', 'Cerrar', { duration: 5000 });
+      }
+    } else {
+      this.snackBar.open('Formulario inválido', 'Cerrar', { duration: 5000 });
+    }
+  }
+
+
+  onNoClick(): void {
+    this.dialogRef.close({ok: false});
+  }
 }
