b74e6aa20f1f7d4d9d39b29f3529a31da12ceba5
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-15 16:47:03 +0100

    user module CHANGE

--- src/app/user-management/user-management.module.ts
+++ src/app/user-management/user-management.module.ts
@@ -6,18 +6,24 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { MaterialModule } from '../material/material.module';
 import { ListUserPageComponent } from './pages/list-user-page/list-user-page.component';
 import { AddUserPageComponent } from './pages/add-user-page/add-user-page.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { EditUsuarioComponent } from './components/edit-usuario/edit-usuario.component';
+import { DeleteUsuarioComponent } from './components/delete-usuario/delete-usuario.component';
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
     ListUserPageComponent,
-    AddUserPageComponent
+    AddUserPageComponent,
+    EditUsuarioComponent,
+    DeleteUsuarioComponent
   ],
   imports: [
     CommonModule,
     UserManagementRoutingModule,
-    MaterialModule
+    MaterialModule,
+    MatPaginatorModule
   ]
 })
 export class UserManagementModule { }
