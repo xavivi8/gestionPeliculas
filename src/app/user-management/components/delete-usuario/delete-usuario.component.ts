1428b79c70ca1f6d3f86ae9a9e95481819cf3185
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-15 16:46:03 +0100

    delete usuario CREATE

---
+++ src/app/user-management/components/delete-usuario/delete-usuario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-usuario',
+  templateUrl: './delete-usuario.component.html',
+  styles: [
+  ]
+})
+export class DeleteUsuarioComponent {
+
+}
