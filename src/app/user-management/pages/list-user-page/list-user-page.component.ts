2c1bec819641c17560d753915fc8b9010d7abd3b
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-15 16:03:20 +0100

    list-user-page ADD

---
+++ src/app/user-management/pages/list-user-page/list-user-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-user-page',
+  templateUrl: './list-user-page.component.html',
+  styles: [
+  ]
+})
+export class ListUserPageComponent {
+
+}
