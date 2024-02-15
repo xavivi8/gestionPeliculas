d006a521749ef24e4d5b0f51c55a8897b15e00e9
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-15 16:02:52 +0100

    add-user-page ADD

---
+++ src/app/user-management/pages/add-user-page/add-user-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-user-page',
+  templateUrl: './add-user-page.component.html',
+  styles: [
+  ]
+})
+export class AddUserPageComponent {
+
+}
