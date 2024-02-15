6e45f56d33e55ebcff233f50f3991b0d0170cd74
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-15 20:30:27 +0100

    delete user WORKS

--- src/app/user-management/components/delete-usuario/delete-usuario.component.ts
+++ src/app/user-management/components/delete-usuario/delete-usuario.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Usuario } from '../../interfaces/usuario.interface';
+import { UserService } from '../../service/user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { firstValueFrom } from 'rxjs';
+import { CLOSE } from 'src/app/shared/messages';
 
 @Component({
   selector: 'app-delete-usuario',
   templateUrl: './delete-usuario.component.html',
-  styles: [
-  ]
+  styleUrls: ['./delete-usuario.component.css']
 })
-export class DeleteUsuarioComponent {
+export class DeleteUsuarioComponent implements OnInit {
+  constructor(public dialogRef: MatDialogRef<DeleteUsuarioComponent>,
+    @Inject(MAT_DIALOG_DATA) public usuario: Usuario,
+    private servicioUsuario: UserService,
+    private snackBar: MatSnackBar,
+  ) { }
 
+  ngOnInit() {
+  }
+
+  async deleteUser() {
+    const RESP = await firstValueFrom(this.servicioUsuario.deleteUsuario(this.usuario));
+    if (RESP && RESP.ok) {
+      this.snackBar.open(RESP.message || '', CLOSE, { duration: 5000 });
+      this.dialogRef.close({ ok: RESP.ok, data: RESP.data });
+    } else {
+      this.snackBar.open(RESP.message || '', CLOSE, { duration: 5000 });
+    }
+  }
+
+  onNoClick() {
+    this.dialogRef.close({ ok: false });
+  }
 }
