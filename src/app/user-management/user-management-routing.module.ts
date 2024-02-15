7aa2b75db4eb5194c71d91b4d296edde6f43e5a5
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-15 17:24:39 +0100

    UserManagementRoutingModule  CHANGE

--- src/app/user-management/user-management-routing.module.ts
+++ src/app/user-management/user-management-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ListUserPageComponent } from './pages/list-user-page/list-user-page.component';
 
 const routes: Routes = [
   {
-    path: '', component: HomePageComponent
+    path: '', component: HomePageComponent,
+    children: [
+      { path: 'home', component: HomePageComponent },
+      { path: 'list', component: ListUserPageComponent }
+    ]
   }
 ];
 
