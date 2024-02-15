98fc767553ced6aad5cbbae6075a0f4ec386896c
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-15 15:13:00 +0100

    module CHANGE

--- src/app/user-management/user-management.module.ts
+++ src/app/user-management/user-management.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserManagementRoutingModule } from './user-management-routing.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent
+  ],
   imports: [
     CommonModule,
     UserManagementRoutingModule
