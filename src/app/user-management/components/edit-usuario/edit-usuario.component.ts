0a0342b8c4a5fbb7a7f3017291b26a5c2c9e14e2
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-15 16:46:16 +0100

    edit usuario CREATE

---
+++ src/app/user-management/components/edit-usuario/edit-usuario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-usuario',
+  templateUrl: './edit-usuario.component.html',
+  styles: [
+  ]
+})
+export class EditUsuarioComponent {
+
+}
