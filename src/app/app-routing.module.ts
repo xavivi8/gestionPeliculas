1047cb87eea77b4386c8f748e74cbe220c15c6a3
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-16 13:28:46 +0100

    AuthGuard IMPLEMENTED

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
+import { AuthGuardService as AuthGuard } from './auth/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule)
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule),
+    canActivate: [AuthGuard],
   },
   {
     path: 'inicio',
-    loadChildren: () => import('./shared/shared.module').then( m => m.SharedModule)
+    loadChildren: () => import('./shared/shared.module').then( m => m.SharedModule),
+    canActivate: [AuthGuard],
   },
   {
     path: 'peliculas',
-    loadChildren: () => import('./peliculas/peliculas.module').then( m => m.PeliculasModule)
-
+    loadChildren: () => import('./peliculas/peliculas.module').then( m => m.PeliculasModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'user-management',
-    loadChildren: () => import('./user-management/user-management.module').then( m => m.UserManagementModule)
+    loadChildren: () => import('./user-management/user-management.module').then( m => m.UserManagementModule),
+    canActivate: [AuthGuard]
   },
   { path: '404', component: Error404PageComponent },
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
