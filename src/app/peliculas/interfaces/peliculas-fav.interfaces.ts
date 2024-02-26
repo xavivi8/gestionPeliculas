fcc241f1c65064b11bafd1b1d8e2e5dccc7c697d
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-26 18:02:35 +0100

    peliculas-fav.interfaces.ts CHANGE

--- src/app/peliculas/interfaces/peliculas-fav.interfaces.ts
+++ src/app/peliculas/interfaces/peliculas-fav.interfaces.ts
@@ -1,3 +1,10 @@
+export interface ResultadoPeliFav{
+  data: PeliFav[],
+  message: string,
+  ok: boolean,
+  permises: string
+}
+
 export interface PeliFav{
   id_pelicula_fav: number;
   usuario: string;
