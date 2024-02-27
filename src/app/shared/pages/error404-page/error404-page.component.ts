f161c6086c647d8f11e4e5e15c652fd831d621c1
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-27 15:52:42 +0100

    error404-page.component.ts CHANGE

--- src/app/shared/pages/error404-page/error404-page.component.ts
+++ src/app/shared/pages/error404-page/error404-page.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-error404-page',
   templateUrl: './error404-page.component.html',
-  styles: [
-  ]
+  styleUrls: ['./error404-page.component.css']
 })
 export class Error404PageComponent {
 
