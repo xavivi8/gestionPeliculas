f01d18f825a38c0aaff3a62d30ab3f6c25bb49f8
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-02-27 11:39:11 +0100

    shared.service.ts ADD  isLoged y isLoged

--- src/app/shared/services/shared.service.ts
+++ src/app/shared/services/shared.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { ApiResponse } from 'src/app/auth/interfaces/usuario.interfaces';
 import { URL_API } from 'src/environments/environments';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class SharedService {
 
   private bearer_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOWExOTJlMDRhMmUxNjQxY2UwM2Q3YmIwZjM0MmQyYSIsInN1YiI6IjY1YzNhOWFlY2ZmZWVkMDE2MzUxNzIzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JZczhan4Mfx7xuuQVTaB0TWdQaI0E4Hsd-CnslPQWYg";
@@ -19,14 +20,14 @@ export class SharedService {
       'Authorization': `Bearer ${this.bearer_token}`
     });
     this.headersSge = new HttpHeaders({
-      'Content-Type':  'application/json',
-      Authorization : `Bearer ${localStorage.getItem('token')}`
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`
     });
   }
 
   getHeaders() {
     return new HttpHeaders({
-      'Content-Type':  'application/json',
+      'Content-Type': 'application/json',
       Authorization: `Bearer ${localStorage.getItem('token')}`
     });
   }
@@ -38,5 +39,32 @@ export class SharedService {
     this.cookieService.deleteAll();
     localStorage.clear();
     return this.http.post(`${URL_API}/logout.php`, body);
+  }
+
+  public async isLoged(): Promise<boolean> {
+    const promise = new Promise<boolean>((resolve, reject) => {
+      this.http.get<ApiResponse>(`${URL_API}/check_usuarios.php?ruta=inicio`, { headers: this.headersSge })
+        .subscribe((response: ApiResponse) => {
+          console.log(response)
+          resolve(response.ok);
+        });
+    });
+    return promise;
+  }
+
+  public async isAuthenticated(url: string): Promise<boolean> {
+    let rutaSeleccionada: string;
+    const promise = new Promise<boolean>((resolve, reject) => {
+
+      rutaSeleccionada = url.substring(1);
+      rutaSeleccionada = rutaSeleccionada.split('/')[0];
+
+      this.http.get<ApiResponse>(`${URL_API}/check_usuarios.php?ruta=${rutaSeleccionada}`, { headers: this.headersSge })
+        .subscribe((response: ApiResponse) => {
+          console.log(response)
+          resolve(response.ok);
+        });
+    });
+    return promise;
   }
 }
