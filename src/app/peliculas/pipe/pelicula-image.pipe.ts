59ba899a722380e73457df0b516cd656c71f8c50
Francisco Javier Martín-Lunas Escobar <jmartinlunasescobar@gmail.com>
2024-03-06 10:46:41 +0100

    pelicula-image.pipe.ts CHANGES

--- src/app/peliculas/pipe/pelicula-image.pipe.ts
+++ src/app/peliculas/pipe/pelicula-image.pipe.ts
@@ -10,14 +10,15 @@ export class PeliculaImagePipe  implements PipeTransform {
   /**
    * Transforma la URL de la imagen de la película.
    * Si la URL es indefinida, devuelve la URL de una imagen de reemplazo por defecto.
-   * @param value La URL de la imagen de la película.
+   * @param {string | null} value La URL de la imagen de la película.
    * @returns La URL completa de la imagen de la película.
    */
-  transform(value: string | undefined): string {
-    if(value === undefined) {
-      return 'assets/no-image.png';
-    }
-    const baseUrl = 'https://media.themoviedb.org/t/p/w300_and_h450_bestv2';
-    return `${baseUrl}${value}`;
+  transform(value : string | null): string {
+      if(value?.endsWith("null")){
+        console.log('noimg')
+        return '/assets/no-image.jpg';
+      }else{
+       return "https://image.tmdb.org/t/p/w500/" + value
+      }
   }
 }
