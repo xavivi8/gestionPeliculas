b655cda02a3a76a37ab4fd1785171ea4ed79b633
xavivi8 <jmartinlunasescobar@gmail.com>
2024-03-06 17:18:53 +0100

    pelicula-image.pipe.ts CHANGE

--- src/app/peliculas/pipe/pelicula-image.pipe.ts
+++ src/app/peliculas/pipe/pelicula-image.pipe.ts
@@ -10,8 +10,8 @@ export class PeliculaImagePipe  implements PipeTransform {
   /**
    * Transforma la URL de la imagen de la película.
    * Si la URL es indefinida, devuelve la URL de una imagen de reemplazo por defecto.
-   * @param {string | null} value La URL de la imagen de la película.
-   * @returns La URL completa de la imagen de la película.
+   * @param {string | null | undefined} value La URL de la imagen de la película.
+   * @returns {string} La URL completa de la imagen de la película.
    */
   transform(value : string | null | undefined): string {
       if(value?.endsWith("null")){
